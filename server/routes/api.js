55fa0a5a5a250ed5765e947e077a22d6858f931e
Anshul Jain <50101299+rayqube@users.noreply.github.com>
2020-01-23 21:02:04 +0530

    change

--- server/routes/api.js
+++ server/routes/api.js
@@ -16,7 +16,7 @@ var token;
 const server = require("../../server");
 var newToken = 123456987;
 
-mongoose.connect('mongodb+srv://<anshuljain@rayqube.com>:<Anshul@123>@shoppingportal-mdtac.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, (err, shoppingportal) => {
+mongoose.connect('mongodb+srv://anshul:anshul21@shoppingportal-mdtac.mongodb.net/test?retryWrites=true&w=majority', { useUnifiedTopology: true, useCreateIndex: true, useNewUrlParser: true }, (err, shoppingportal) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
